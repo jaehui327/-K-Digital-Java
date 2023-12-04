d4fa0d747ecdcd4c04dff8fb51c6c8d022f5c391
jaehui327 <jaeheekim970327@gmail.com>
2023-12-04 16:50:07 +0900

    [Update] IntelliJ setting

---
+++ src/baekjoon/Main_7569_토마토.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_7569_토마토 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		
+		int[][] map = new int[N][M];
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+			}
+		}
+		br.close();
+	}
+
+}
